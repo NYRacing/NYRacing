fc772715542854c33a136ae11f7af0943a7521d5
YangJiaRui <1204510121@qq.com>
2024-09-07 13:55:29 +0800

    develop_developsoftware_update

---
+++ 2024/Develop Software/VxSpdEsti/VxSpdEsti0713_ert_rtw/rtGetNaN.h
@@ -0,0 +1,29 @@
+/*
+ * File: rtGetNaN.h
+ *
+ * Code generated for Simulink model 'VxSpdEsti0713'.
+ *
+ * Model version                  : 1.37
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Sat Jul 13 10:27:14 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtGetNaN_h_
+#define RTW_HEADER_rtGetNaN_h_
+#include "rtwtypes.h"
+
+extern real_T rtGetNaN(void);
+extern real32_T rtGetNaNF(void);
+
+#endif                                 /* RTW_HEADER_rtGetNaN_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
